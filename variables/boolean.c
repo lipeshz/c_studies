9605403b798e7c68b176e581f042a3b07eb15115
lipeshz <filipeglima2005@gmail.com>
2026-07-03 15:26:50 -0300

    variables

--- variables/boolean.c
+++ variables/boolean.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
-#include <sdtbool.h>
+#define bool int
+#define true 1
+#define false 0
 
 int main(){
+    // boolean stores true or false (also means 1 or 0)
+    // format specifier: %d
+    bool isOnline = 1;
 
+    if(isOnline){
+        printf("You are ONLINE.");
+    }else{
+        printf("You are OFFLINE");
+    }
+    
     return 0;
 }
